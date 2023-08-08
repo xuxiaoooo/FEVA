1a6823583abe9ff5e105cba7d679b5d76e37836c
xuxiaoooo <1078446778@qq.com>
2023-08-08 18:46:12 +0800

    😎

--- utils/LAM.py
+++ utils/LAM.py
@@ -6,7 +6,7 @@ from All_MLPBlock import AddNorm, FeedForward, PredictionLayer
 class MyModel(nn.Module):
     def __init__(self, d_model, d_ff, interval=None, percentage=None):
         super(MyModel, self).__init__()
-        self.act = Activation(interval, percentage)  # Add the activation module
+        self.act = Activation(interval=4)
         self.addnorm1 = AddNorm(d_model)
         self.ff = FeedForward(d_model, d_ff)
         self.addnorm2 = AddNorm(d_model)
@@ -16,10 +16,8 @@ class MyModel(nn.Module):
     def forward(self, x, ei):
         results = []
         for segment in x:
-            # Apply activation
             out = self.act(segment)
 
-            # Apply Add & Norm, Feed-forward, Add & Norm
             out = self.addnorm1(out, self.ff(out))
             out = self.addnorm2(out, self.ff(out))
 
